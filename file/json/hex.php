be619fe1b00298f25b19460f505547261c359d76
hamaiosdev <69496990+hamaiosdev@users.noreply.github.com>
2021-05-05 02:10:54 +0300

    Update hex.php

--- file/json/hex.php
+++ file/json/hex.php
@@ -1,8 +1,9 @@
-var obj = JSON.parse($reponse.body); 
+var obj = JSON.parse($response.body);
+
 obj= {
   "messages":
   ["[Serial No: KR2104MA0805k0000]","[Pin: 60292455161234]", "کلیک بکە لەسەر *٢٢١*ژمارەی نهێنی #بۆ پڕکردنەوەی خێرا"],
   "data": "KR2104MA0805k0000  60292455161234", 
   "code": 200
 };
- $done({body: JSON.stringify(obj)});
+$done({body: JSON.stringify(obj)});
